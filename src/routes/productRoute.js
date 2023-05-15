0ab4b85a272f5ece2da478c966002a330b0ac747
UmerAliShah <muhammadumeralishah@gmail.com>
2023-05-15 20:34:48 +0500

    changes added

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -14,6 +14,6 @@ const router = express.Router();
 router.post("/", requireAuth, upload.single("image"), addProduct);
 router.put("/:id", requireAuth, upload.single("image"), putProduct);
 router.delete("/:id", requireAuth, deleteProduct);
-router.get("/", getProduct);
+router.get("/all", getProduct);
 router.get("/getVerifiedProduct/:pc", getVerifiedProduct);
 module.exports = router;
